f062edaa5c0d33e812a3ea81f64aa969f88e9c24
Maz Akbar <akbarmas973@gmail.com>
2022-01-01 18:54:34 +0700

    main  vess1

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Ini adalah proyek pembuka remote python
+"""
+print ("Hello world..")
+print ("My name is Akbar")
+print ("I was starting coding with python,")
+print ("although some place here is rainy this season")
